29e96be5c3a736ed59ef1c321423222f4da3d9fb
nickporohovoy <cosmosvnutri8@mail.ru>
2023-04-23 19:54:18 +0400

    seminar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"your input {num} number");
+
+// Tфsk1. Запросить число и вывести его квадрат.
+
+// Console.WriteLine("input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Quad of {num} -> {num*num}");
+
+// Task2. Является ли первое число квадратом второго?
+
+// Console.WriteLine("input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 == num2*num2)
+// {
+//     Console.WriteLine($"{num1} является квадратом {num2}");
+// }
+// else
+// {
+//     Console.WriteLine($"{num1} не является квадратом {num2}");
+// }
+
+// Task3. Напишите программу которая принимает на вход число N, на выходе получая последовательность от -N до N
+
+// Console.WriteLine("input number");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if(n<0) Console.WriteLine("inval");
+// int count = n*(-1);
+// while(count <= n)
+// {
+//     Console.Write(count+" ");
+//     count++;
+// }
+
+// Task4. На вход принимаем 3х значное число, вывести последнюю цифру данного числа.
+
+// Console.WriteLine("input three-digit number");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// // 333 % 10
+
+// // int ed = n % 10;
+// Console.WriteLine($"Last digit of {n} -> {n%10}");
